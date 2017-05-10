85e8862575cb165183db16c8f2d9b8e9a26d8072
Jan G. Wieners <jan@jan-wieners.de>
2017-05-10 12:51:01 +0200

    Add leaflet map.

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import 'leaflet';
+
 @IonicPage()
 @Component({
   selector: 'page-map',
@@ -12,7 +14,15 @@ export class MapPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
+
+    let map = L.map('map')
+      .setView([50.9213393, 6.9184396], 3);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      accessToken: 'pk.eyJ1IjoiZG9jZm5vcmQiLCJhIjoiY2oyaXVvbWJoMDAyaTJ3bnMwa3N2Ynp0cCJ9.r2ZDHVEmSv2y9XwZzjnjWw'
+    }).addTo(map);
   }
 
 }
