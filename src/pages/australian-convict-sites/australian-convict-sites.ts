5d194e6b7f95490b61ab8232d829dd9ade064ec8
Jan G. Wieners <jan@jan-wieners.de>
2017-08-17 17:04:11 +0200

    Fill page "australian convict sites" with content, implement ux.

--- src/pages/australian-convict-sites/australian-convict-sites.ts
+++ src/pages/australian-convict-sites/australian-convict-sites.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, Content } from 'ionic-angular';
+import {SafeResourceUrl, DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'page-australian-convict-sites',
@@ -7,7 +8,17 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class AustralianConvictSitesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild(Content) content: Content;
+
+  private videoUrl: SafeResourceUrl;
+
+  scrollTo(element:string) {
+    let yOffset = document.getElementById(element).offsetTop;
+    this.content.scrollTo(0, yOffset, 3000)
+  }
+
+  constructor(private domSanitizer: DomSanitizer, public navCtrl: NavController, public navParams: NavParams) {
+    this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/Ilw45DNuRWI')
   }
 
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -5,12 +5,14 @@ import {HelpPage} from "../help/help";
 import {OverviewPage} from '../overview/overview';
 import {CodescannerPage} from "../codescanner/codescanner";
 import {TourPage} from "../tour/tour";
+import {AustralianConvictSitesPage} from "../australian-convict-sites/australian-convict-sites";
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
+  starter = AustralianConvictSitesPage;
   tourRoot = TourPage;
   tab1Root = HelpPage;
   tab2Root = InfoPage;
