a5bc52f8fd43285d28a5d64d9b540ffb982a1efe
Jan G. Wieners <jan@jan-wieners.de>
2017-08-20 11:42:29 +0200

    Fill page "Ritual Ceremony of the Voladores" with content.

--- src/pages/los-voladores/los-voladores.ts
+++ src/pages/los-voladores/los-voladores.ts
@@ -18,7 +18,7 @@ export class LosVoladoresPage {
   }
 
   constructor(private domSanitizer: DomSanitizer, public navCtrl: NavController, public navParams: NavParams) {
-    this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/Ilw45DNuRWI')
+    this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/GYKF1PAyZ5o')
   }
 
 }
