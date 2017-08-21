acef104c15b7c50378c4074ec55d05a7ef5b75a2
Jan G. Wieners <jan@jan-wieners.de>
2017-08-21 18:24:41 +0200

    Add content.

--- src/pages/hoi-an/hoi-an.ts
+++ src/pages/hoi-an/hoi-an.ts
@@ -18,7 +18,7 @@ export class HoiAnPage {
   }
 
   constructor(private domSanitizer: DomSanitizer, public navCtrl: NavController, public navParams: NavParams) {
-    this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/Ilw45DNuRWI')
+    this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/suSXZe1Vq8Q')
   }
 
 }
