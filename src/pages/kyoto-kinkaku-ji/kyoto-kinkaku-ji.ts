7fb30fbda3f765618ca4dc4babc3a157a82e6c34
Jan G. Wieners <jan@jan-wieners.de>
2017-08-21 10:13:34 +0200

    Add content.

--- src/pages/kyoto-kinkaku-ji/kyoto-kinkaku-ji.ts
+++ src/pages/kyoto-kinkaku-ji/kyoto-kinkaku-ji.ts
@@ -18,7 +18,7 @@ export class KyotoKinkakuJiPage {
   }
 
   constructor(private domSanitizer: DomSanitizer, public navCtrl: NavController, public navParams: NavParams) {
-    this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/Ilw45DNuRWI')
+    this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/gko9NpmG94w')
   }
 
 }
